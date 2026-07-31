3f0ebb31d4fa469ebf12712b77dc05ef6a3dfba8
Kevin Chi <kevinfabian2911@gmail.com>
2026-07-30 20:32:30 -0600

    fix: implementar FilamentUser para dar acceso al panel en producción

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,7 +13,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
 
@@ -86,5 +88,13 @@ class User extends Authenticatable
             ->take(2)
             ->map(fn (string $word): string => Str::substr($word, 0, 1))
             ->implode('');
+    }
+
+    /**
+     * Determina si el usuario puede acceder al panel de Filament.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
